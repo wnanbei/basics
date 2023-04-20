f00dc93a89706fd8524b559f5579c6709fd739ef
wnanbei <wnanbei@gmail.com>
2023-04-20 17:57:47 +0800

    feat: mapstructure tag

--- config/model.go
+++ config/model.go
@@ -3,18 +3,18 @@ package config
 // Config 通用配置模版
 type Config struct {
 	Server Server `json:"server" mapstructure:"server"` // 服务端配置
-	MySQL  MySQL  `json:"mysql"`                        // MySQL 数据库配置
-	Redis  Redis  `json:"redis"`                        // Redis 数据库配置
+	MySQL  MySQL  `json:"mysql" mapstructure:"mysql"`   // MySQL 数据库配置
+	Redis  Redis  `json:"redis" mapstructure:"redis"`   // Redis 数据库配置
 }
 
 // Server 常用配置
 type Server struct {
-	Host          string `json:"host" mapstructure:"host"` // host
-	Port          string `json:"port" mapstructure:"port"` // 端口
-	Version       string `json:"version"`                  // 服务版本
-	BasePath      string `json:"base_path"`                // 根路径
-	Env           Env    `json:"env"`                      // 所属环境
-	EnableSwagger bool   `json:"enable_swagger"`           // 是否开启 swagger 文档
+	Host          string `json:"host" mapstructure:"host"`                     // host
+	Port          string `json:"port" mapstructure:"port"`                     // 端口
+	Version       string `json:"version" mapstructure:"version"`               // 服务版本
+	BasePath      string `json:"base_path" mapstructure:"base_path"`           // 根路径
+	Env           Env    `json:"env" mapstructure:"env"`                       // 所属环境
+	EnableSwagger bool   `json:"enable_swagger" mapstructure:"enable_swagger"` // 是否开启 swagger 文档
 }
 
 // Env 运行环境
@@ -28,17 +28,17 @@ const (
 
 // MySQL 数据库配置
 type MySQL struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
-	Database string `json:"database"`
+	Host     string `json:"host" mapstructure:"host"`
+	Port     string `json:"port" mapstructure:"port"`
+	UserName string `json:"user_name" mapstructure:"user_name"`
+	Password string `json:"password" mapstructure:"password"`
+	Database string `json:"database" mapstructure:"database"`
 }
 
 // Redis 数据库配置
 type Redis struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	Password string `json:"password"`
-	Database string `json:"database"`
+	Host     string `json:"host" mapstructure:"host"`
+	Port     string `json:"port" mapstructure:"port"`
+	Password string `json:"password" mapstructure:"password"`
+	Database string `json:"database" mapstructure:"database"`
 }
